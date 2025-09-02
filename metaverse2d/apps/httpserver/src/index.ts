245eed21302e5bf60a13791496a52a6f4354a65d
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2025-09-03 02:07:20 +0530

    prisma setup done...also written database schema

---
+++ metaverse2d/apps/httpserver/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors";
+import { v1Router } from "./router/v1/index.js";
+import client from "@metaverse2d/database";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+client.$connect();
+// defining prefix rotuers ...
+app.use("api/v1", v1Router);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
