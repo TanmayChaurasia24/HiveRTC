8283287fef6baf0394af2a12992b87299c2fe8e8
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2025-09-21 16:10:54 +0530

    written welcome route

---
+++ apps/webRTC/internal/handlers/welcome.go
@@ -0,0 +1,9 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func Welcome(c *fiber.Ctx) error {
+	return c.SendString("Welcome to the metaverse webRTC server!")
+}
--- apps/webRTC/internal/server/server.go
+++ apps/webRTC/internal/server/server.go
@@ -2,19 +2,26 @@ package server
 
 import (
 	"log"
+	"time"
+
+	"webRTC/internal/handlers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/websocket/v2"
 )
 
 func Run() error {
 	app := fiber.New()
 
+	app.Use(logger.New()) // Logs every request
+	app.Use(cors.New())
+
 	// health check point
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.SendString("health check!")
 	})
-
 
 	// Example routes (replace with your actual handler funcs)
 	app.Get("/", handlers.Welcome)
@@ -22,7 +29,9 @@ func Run() error {
 	app.Get("/room/:room", handlers.JoinRoom)
 
 	// WebSocket routes
-	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWS))
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWS, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+	}))
 	app.Get("/room/:uuid/chat", handlers.RoomChat)
 	app.Get("/room/:uuid/chat/websocket", websocket.New(handlers.RoomChatWS))
 	app.Get("/room/:uuid/viewer/websocket", websocket.New(handlers.RoomViewerWS))
